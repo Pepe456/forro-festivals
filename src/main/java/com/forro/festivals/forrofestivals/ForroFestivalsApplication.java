690950943781cf188b1050e955fb42b5b374db24
Sebastian Kneise <sebastian.kneise@andrena.de>
2018-05-25 21:13:34 +0200

    init

---
+++ src/main/java/com/forro/festivals/forrofestivals/ForroFestivalsApplication.java
@@ -0,0 +1,12 @@
+package com.forro.festivals.forrofestivals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForroFestivalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForroFestivalsApplication.class, args);
+	}
+}
---
+++ src/test/java/com/forro/festivals/forrofestivals/ForroFestivalsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.forro.festivals.forrofestivals;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ForroFestivalsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
