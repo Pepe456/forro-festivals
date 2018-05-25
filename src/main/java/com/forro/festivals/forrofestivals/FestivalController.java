1f28f22f421d1e2e3b6c1a67d592ac48d4e2733d
Sebastian Kneise <sebastian.kneise@andrena.de>
2018-05-25 21:54:55 +0200

    init2

---
+++ src/main/java/com/forro/festivals/forrofestivals/FestivalController.java
@@ -0,0 +1,18 @@
+package com.forro.festivals.forrofestivals;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/")
+public class FestivalController {
+
+    public FestivalController() {
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "festivals")
+    public String getFestivals() {
+        return "Festivals";
+    }
+}
